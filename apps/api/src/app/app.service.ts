09c2aad58928b9ec5f80992aad3318ee8638bce8
danikaze <danikaze@gmail.com>
2024-02-03 15:51:57 +0900

    Basic API behavior

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { getGameData } from '@game';
+import { encodeBinaryMatrix } from '@utils/binary-matrix';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  getData() {
+    const data = getGameData();
+
+    if (!data) return;
+
+    return {
+      kanas: data.kanas,
+      words: data.words,
+      matrix: encodeBinaryMatrix(data.matrix, (char) => char !== ''),
+    };
   }
 }
