bc05d09bd84725d55766daad059ad5e67afebd21
danikaze <danikaze@gmail.com>
2024-06-15 01:50:14 +0900

    Provide AppOptions as a Context

--- apps/web/app/hana/page.tsx
+++ apps/web/app/hana/page.tsx
@@ -1,11 +1,14 @@
 import { HanaGame } from '../../components/hana-game';
+import { OptionsProvider } from '../../components/options/context';
 
 import styles from '../page.module.scss';
 
 export default async function HanaPage() {
   return (
     <div className={styles.page}>
-      <HanaGame />
+      <OptionsProvider>
+        <HanaGame />
+      </OptionsProvider>
     </div>
   );
 }
